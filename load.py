c8c496a388563e14a22685954140a342227231c3
HarryEP <44836842+HarryEP@users.noreply.github.com>
2023-09-05 14:32:52 +0100

    retrieve the csv data and make it into a dataframe

--- load.py
+++ load.py
@@ -6,13 +6,22 @@ from psycopg2.extras import RealDictCursor
 from psycopg2.extensions import connection
 from dotenv import load_dotenv
 
+CODES_CSV = "cancel_codes.csv"
 
-def write_cancel_codes(conn: connection):
-    pass
+
+def write_cancel_codes(conn: connection, codes_df: pd.DataFrame):
+    '''writes the cancel codes from the dataframe to the database'''
+    records = cancel_codes_df.to_records(index=False)
+    with conn.cursor() as cur:
+        sql_query = '''
+            INSERT INTO '''
+        cur.executemany(sql_query, records)
 
 
 def get_cancel_code_csv_data() -> pd.DataFrame:
-    pass
+    '''extracts the cancel codes from the csv to a dataframe'''
+    cancel_codes_df = pd.read_csv(CODES_CSV)
+    return cancel_codes_df
 
 
 def get_connection(host: str, db_name: str, password: str, user: str):
@@ -26,6 +35,9 @@ def get_connection(host: str, db_name: str, password: str, user: str):
 
 
 if __name__ == "__main__":
+
     load_dotenv()
     conn = get_connection(os.environ["DB_HOST"], os.environ["DB_NAME"],
                           os.environ["DB_PASS"], os.environ["DB_USER"])
+    cancel_codes_df = get_cancel_code_csv_data()
+    write_cancel_codes(conn, cancel_codes_df)
