11d32930b25425b1bc3624eabcb6a541f68324e9
Annie Mahmood <anniemahmood@Annies-MBP.cust.communityfibre.co.uk>
2023-09-05 13:36:04 +0100

    fixed pylint issues

--- transform.py
+++ transform.py
@@ -11,26 +11,28 @@ def load_data(csv_path: str) -> DataFrame:
     return the data as a DataFrame
     """
     try:
-        with open(csv_path, 'r') as file:
-            data = pd.read_csv(csv_path)
-            return data
+        data = pd.read_csv(csv_path)
+        return data
 
-    except (FileNotFoundError) as e:
-        print(f"Error loading .csv data: {e}")
+    except FileNotFoundError:
+        print("Error loading .csv data: FileNotFound")
         return None
-    
 
-def hhmm_to_timestamp(hhmmss_time_string: str):
+
+def hhmmss_to_timestamp(time_string: str):
     """
     Takes a 'time' string in the form
     'HHMMSS' and returns a valid timestamp
     """
     try:
-        hh = int(hhmmss_time_string[:2])
-        mm = int(hhmmss_time_string[2:4])
-        ss = int(hhmmss_time_string[4:])
-        dt = datetime.now().replace(hour=hh, minute=mm, second=ss, microsecond=0)
-        timestamp = dt.timestamp()
+        hh_value = int(time_string[:2])
+        mm_value = int(time_string[2:4])
+        ss_value = int(time_string[4:])
+        datetime_value = datetime.now().replace(hour=hh_value,
+                                                minute=mm_value,
+                                                second=ss_value,
+                                                microsecond=0)
+        timestamp = datetime_value.timestamp()
         return timestamp
 
     except ValueError:
@@ -49,30 +51,36 @@ def create_timestamp_from_date_and_time(df: DataFrame, new_column_name: str,
     try:
         df[date_column_name] = pd.to_datetime(df[date_column_name], format='%Y-%m-%d')
 
-    except ValueError as e:
-        print(f"Error: invalid values in date column")
+    except ValueError:
+        print("Error: invalid values in date column")
         return None
-    
+
     try:
         df[time_column_name] = pd.to_datetime(df[time_column_name], format='%H%M%S')
-    
-    except ValueError as e:
-        print(f"Error: invalid values in time column")
+
+    except ValueError:
+        print("Error: invalid values in time column")
         return None
-    
-    df[new_column_name] = df[date_column_name] + pd.to_timedelta(df[time_column_name].dt.strftime('%H:%M:%S'))
+
+    df[new_column_name] = df[date_column_name] + pd.to_timedelta(
+        df[time_column_name].dt.strftime('%H:%M:%S'))
     return df
 
 
 if __name__ == "__main__":
 
-    csv_path = ""
+    input_csv_path = ""
 
-    service_df = load_data(csv_path)
+    service_df = load_data(input_csv_path)
 
-    service_df = create_timestamp_from_date_and_time(service_df, "scheduled_arrival_datetime",
-                                                     "scheduled_arrival_date", "scheduled_arrival_time")
-    
+    service_df = create_timestamp_from_date_and_time(service_df,
+                                                     "scheduled_arrival_datetime",
+                                                     "scheduled_arrival_date",
+                                                     "scheduled_arrival_time")
+
     service_df = create_timestamp_from_date_and_time(service_df, "origin_run_datetime",
                                                      "origin_run_date", "origin_run_time")
-    
+
+    output_csv_path = "transformed_service_data.csv"
+
+    service_df.to_csv(output_csv_path)
