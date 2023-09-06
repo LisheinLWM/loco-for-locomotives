94fa97f6bc187a19a6870a2180655664c464edb8
Lishein <trainee.lishein.wmsigmalabs.co.uk>
2023-09-06 11:40:19 +0100

    function to add company names in to company table and function to switch between short and long term schema

--- pipeline/load.py
+++ pipeline/load.py
@@ -13,7 +13,8 @@ CODES_CSV = "cancel_codes.csv"
 
 
 def get_connection(host: str, db_name: str, password: str, user: str):
-    '''Connects to the database'''
+    """Connects to the database"""
+
     try:
         conn = psycopg2.connect(host=host,
                                 dbname=db_name,
@@ -25,15 +26,35 @@ def get_connection(host: str, db_name: str, password: str, user: str):
         print(f"Error {e} occured!")
 
 
+def switch_between_schemas(schema_name: str) -> None:
+    """Switches to the schema by the schema name provided"""
+
+    with conn.cursor() as cur:
+        cur.execute("SET search_path TO %s", (schema_name,))
+    conn.commit()
+
+
 def write_cancel_codes(conn: connection, codes_df: pd.DataFrame):
-    '''writes the cancel codes from the dataframe to the database'''
+    """writes the cancel codes from the dataframe to the database"""
+
     records = codes_df.to_records(index=False)
     with conn.cursor() as cur:
         sql_query = '''
             INSERT INTO cancel_code (code, reason, abbreviation)
-            VALUES (%s, %s, %s)'''
+            VALUES (%s, %s, %s) ON CONFLICT DO NOTHING;'''
         cur.executemany(sql_query, records)
 
+    conn.commit()
+
+
+def insert_company_data(conn: connection, data: pd.DataFrame) -> None:
+    """Inserts company data to the database"""
+
+    company_names = data[['company_name']].values.tolist()
+
+    with conn.cursor() as cur:
+        cur.executemany("""INSERT INTO company (company_name) VALUES
+                        (%s) ON CONFLICT DO NOTHING;""", company_names)
     conn.commit()
 
 
@@ -42,5 +63,10 @@ if __name__ == "__main__":
     load_dotenv()
     conn = get_connection(os.environ["DB_HOST"], os.environ["DB_NAME"],
                           os.environ["DB_PASS"], os.environ["DB_USER"])
+
+    switch_between_schemas("previous_day_data")
     cancel_codes_df = pd.read_csv(CODES_CSV)
     write_cancel_codes(conn, cancel_codes_df)
+
+    data = pd.read_csv("transformed_service_data.csv")
+    insert_company_data(conn, data)
