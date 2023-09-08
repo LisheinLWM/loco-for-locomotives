93b792e1a0632ce2eabfa50e2c61808a54c93916
Ngawang Tsering <ngawangtsering@Ngawangs-MacBook-Pro.local>
2023-09-08 11:06:07 +0100

    updated formatting and pylint

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,4 +1,5 @@
-"""Streamlit dashboard application code
+"""
+Streamlit dashboard application code.
 
 Module contains code for connecting to postgres database (RDS)
 and using that data to create charts for data analysis.
@@ -39,7 +40,9 @@ CSV_COLUMNS = [
 
 
 def get_db_connection():
-    """Establishes a connection with the PostgreSQL database."""
+    """
+    Establishes a connection with the PostgreSQL database.
+    """
     try:
         conn = connect(
             user=environ.get("DATABASE_USERNAME"),
@@ -53,8 +56,11 @@ def get_db_connection():
         print("Error connecting to database: ", err)
         sys.exit()
 
+
 def get_live_database(conn: connection) -> pd.DataFrame:
-    """Retrieve plant data from the database and return as a DataFrame."""
+    """
+    Retrieve plant data from the database and return as a DataFrame.
+    """
 
     query = """
     SET search_path TO previous_day_data;
@@ -101,20 +107,26 @@ LEFT JOIN cancel_code cc ON cn.cancel_code_id = cc.cancel_code_id;
 
 
 def dashboard_header() -> None:
-    """Creates a header for the dashboard and title on tab."""
+    """
+    Creates a header for the dashboard and title on tab.
+    """
 
     st.title("LOCO_FOR_LOCOMOTIVES")
     st.markdown("An app for analysing your train services")
 
 
 def plot_average_delays_by_company(data_df: pd.DataFrame) -> None:
-    """Create a bar chart showing the average delays for each company"""
+    """
+    Create a bar chart showing the average delays for each company.
+    """
     st.title("Average_delays_by_company")
-    # Group the data by company_name and calculate the average delay
-    average_delays = data_df.groupby('company_name')['arrival_lateness'].mean().reset_index()
+
+    average_delays = data_df.groupby('company_name')[
+        'arrival_lateness'].mean().reset_index()
 
     plt.figure(figsize=(15, 6))
-    ax = sns.barplot(x='company_name', y='arrival_lateness', data=average_delays)
+    ax = sns.barplot(x='company_name', y='arrival_lateness',
+                     data=average_delays)
     plt.xlabel('Company Name')
     plt.ylabel('Average Delay')
     plt.title('Average Delays by Company')
@@ -130,7 +142,9 @@ def plot_average_delays_by_company(data_df: pd.DataFrame) -> None:
 
 
 def plot_average_delays_by_station(data_df: pd.DataFrame, station) -> None:
-    """Create a bar chart showing the average delays for each station"""
+    """
+    Create a bar chart showing the average delays for each station.
+    """
     st.title("Average_delays_by_station (TOP 20)")
 
     max_stations = 20
@@ -144,20 +158,22 @@ def plot_average_delays_by_station(data_df: pd.DataFrame, station) -> None:
         data_df = data_df[data_df['origin_station_name'].isin(station)]
 
     # Group the data by origin_station_name and calculate the average delay
-    average_delays = data_df.groupby('origin_station_name')['arrival_lateness'].mean().reset_index()
+    average_delays = data_df.groupby('origin_station_name')[
+        'arrival_lateness'].mean().reset_index()
 
-     # Sort the data by average delay in descending order and select the top 20 stations
-    average_delays = average_delays.sort_values(by='arrival_lateness', ascending=False).head(20)
+    # Sort the data by average delay in descending order and select the top 20 stations
+    average_delays = average_delays.sort_values(
+        by='arrival_lateness', ascending=False).head(20)
 
     plt.figure(figsize=(15, 6))
-    ax = sns.barplot(x='origin_station_name', y='arrival_lateness', data=average_delays)
+    ax = sns.barplot(x='origin_station_name',
+                     y='arrival_lateness', data=average_delays)
     plt.xlabel('Station Name')
     plt.ylabel('Average Delay')
     plt.title('Average Delays by Station')
     plt.xticks(rotation=45)
     plt.tight_layout()
 
-    # Add values as text labels on each bar with 2 decimal places
     for p in ax.patches:
         ax.annotate(f"{p.get_height():.2f} min", (p.get_x() + p.get_width() / 2., p.get_height()),
                     ha='center', va='center', fontsize=10, color='black', xytext=(0, 5),
@@ -166,14 +182,15 @@ def plot_average_delays_by_station(data_df: pd.DataFrame, station) -> None:
 
 
 if __name__ == "__main__":
-    st.set_page_config(page_title="Train Services Monitoring Dashboard", layout="wide")
+    st.set_page_config(
+        page_title="Train Services Monitoring Dashboard", layout="wide")
     load_dotenv()
     connection = get_db_connection()
     database_df = get_live_database(connection)
-    # print(database_df)
+
     dashboard_header()
     selected_station = st.sidebar.multiselect(
-         "Station", options=database_df["origin_station_name"].unique())
+        "Station", options=database_df["origin_station_name"].unique())
+
     plot_average_delays_by_station(database_df, selected_station)
     plot_average_delays_by_company(database_df)
-   
