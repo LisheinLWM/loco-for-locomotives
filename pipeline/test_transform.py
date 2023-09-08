92defc0a418fee3797d4429b2ea4a5c76b1fa5ce
HarryEP <44836842+HarryEP@users.noreply.github.com>
2023-09-08 12:15:45 +0100

    conftest, generated_df implemented

--- pipeline/test_transform.py
+++ pipeline/test_transform.py
@@ -14,51 +14,35 @@ from transform import (
 )
 
 
-def test_data_is_loaded():
+def test_data_is_loaded(generated_df):
     """Tests that the data is loaded correctly"""
-    data = generate_test_dataframe_with_date_and_time_columns()
-    data.to_csv('test_data.csv', index=False)
+    generated_df.to_csv('test_data.csv', index=False)
     result_1 = load_data('test_data.csv')
     assert isinstance(result_1, DataFrame)
     result_2 = load_data('non-existent-file.csv')
     assert result_2 is None
 
 
-def generate_test_dataframe_with_date_and_time_columns():
-    """
-    Generates a DataFrame with a column of
-    date strings and a column of time strings
-    """
-    data = {"date_column": ["2023-09-05", "2023-09-06", "2023-09-08"],
-            "time_column": ["123045", "081530", "153024"],
-            "cancel_code": ["AA", "tabbycat", "ZZ"],
-            "numbers": [12, -5, "cancelled at origin"],
-            "crs": ["ABC", 0, "FUDGE"]}
-    return pd.DataFrame(data)
-
-
-def test_create_timestamp_creates_new_column():
+def test_create_timestamp_creates_new_column(generated_df):
     """
     Tests that create_timestamp_from_date_and_time()
     successfully adds the specified column to the 
     DataFrame
     """
-    df = generate_test_dataframe_with_date_and_time_columns()
     new_column_name = "datetime"
-    df = create_timestamp_from_date_and_time(df, new_column_name,
+    df = create_timestamp_from_date_and_time(generated_df, new_column_name,
                                              "date_column", "time_column")
     assert new_column_name in df
     print(df.dtypes[new_column_name])
 
 
-def test_create_timestamp_new_column_has_datetime_values():
+def test_create_timestamp_new_column_has_datetime_values(generated_df):
     """
     Tests that each value in the column
     created by create_timestamp_from_date_and_time()
     is a datetime value
     """
-    df = generate_test_dataframe_with_date_and_time_columns()
-    df = create_timestamp_from_date_and_time(df, "datetime",
+    df = create_timestamp_from_date_and_time(generated_df, "datetime",
                                              "date_column", "time_column")
     assert df.dtypes["datetime"] == "datetime64[ns]"
 
@@ -87,14 +71,13 @@ def test_create_timestamp_raises_error_if_invalid_values_given():
     assert df_2 is None
 
 
-def test_create_timestamp_generates_correct_values():
+def test_create_timestamp_generates_correct_values(generated_df):
     """
     Tests that create_timestamp_from_date_and_time()
     is creating datetime objects in the new column,
     with the required format (YYYY-MM-DD HH:MM:SS)
     """
-    df = generate_test_dataframe_with_date_and_time_columns()
-    df = create_timestamp_from_date_and_time(df, "datetime",
+    df = create_timestamp_from_date_and_time(generated_df, "datetime",
                                              "date_column", "time_column")
 
     datetime_value = df.loc[0, "datetime"]
@@ -116,31 +99,30 @@ def test_cancel_code_list_generation(cancel_codes_df):
     assert valid_code_list == ['AA', 'AC', 'AD', 'ZZ']
 
 
-def test_valid_and_invalid_cancel_codes_are_processed_correctly(cancel_codes_df):
+def test_valid_and_invalid_cancel_codes_are_processed_correctly(cancel_codes_df, generated_df):
     """Tests that the validity of codes is processed correctly"""
     cancel_codes_df.to_html('mock_cancel_codes.html',
                             classes='wikitable', index=False, border=3, justify='center')
     valid_code_list = generate_list_of_valid_cancel_codes(
         'mock_cancel_codes.html')
-    service_df = generate_test_dataframe_with_date_and_time_columns()
+    service_df = generated_df
     service_df = determine_if_cancel_code_is_valid(service_df, valid_code_list)
     assert service_df['cancel_code'].tolist() == ["AA", None, "ZZ"]
 
 
-def test_numbers_are_processed_correctly():
+def test_numbers_are_processed_correctly(generated_df):
     """Tests that numbers are processed correctly"""
-    df = generate_test_dataframe_with_date_and_time_columns()
-    df = replace_non_integers_with_none(df, "numbers")
+    df = replace_non_integers_with_none(generated_df, "numbers")
     assert df["numbers"].tolist() == [12.0, -5.0, None]
 
 
-def test_CRS_values_are_confirmed_to_be_three_characters():
+def test_CRS_values_are_confirmed_to_be_three_characters(generated_df):
     """Tests that CRS values are 3 characters exactly"""
-    df_1 = generate_test_dataframe_with_date_and_time_columns()
+    df_1 = generated_df
     df_1 = check_values_in_column_have_three_characters(
         df_1, "crs", drop_row=True)
     assert df_1["crs"].tolist() == ["ABC"]
-    df_2 = generate_test_dataframe_with_date_and_time_columns()
+    df_2 = generated_df
     df_2 = check_values_in_column_have_three_characters(
         df_2, "crs", drop_row=False)
-    assert df_1["crs"].tolist() == ["ABC", None, None]
+    assert df_2["crs"].tolist() == ["ABC", None, None]
