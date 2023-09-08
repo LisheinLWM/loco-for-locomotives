87fd50e6707355f144889e2ee45307d74448c95a
HarryEP <44836842+HarryEP@users.noreply.github.com>
2023-09-08 15:07:59 +0100

    testing switch_between_schemas

--- pipeline/test_load.py
+++ pipeline/test_load.py
@@ -1,11 +1,23 @@
 from unittest.mock import patch, MagicMock
-from load import write_cancel_codes
+from load import write_cancel_codes, switch_between_schemas
 
 
-def test_write_cancel_codes():
+# def test_write_cancel_codes(cancel_codes_df):
+
+#     fake_connection = MagicMock()
+
+#     fake_execute = fake_connection.cursor().execute
+
+#     write_cancel_codes(fake_connection, cancel_codes_df)
+
+#     assert fake_execute.call_count == 1
+
+def test_switch_between_schema(cancel_codes_df):
 
     fake_connection = MagicMock()
 
     fake_execute = fake_connection.cursor().execute
 
-    write_cancel_codes(fake_connection, ['a', 'b', 'c'])
+    switch_between_schemas(fake_connection, 'Lishein')
+
+    assert fake_execute.call_count == 1
