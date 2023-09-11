8f06941c58efbe531d693c20fd387ba1955e4948
Annie Mahmood <anniemahmood@Annies-MBP.cust.communityfibre.co.uk>
2023-09-11 10:07:54 +0100

    added incident pipeline folder

---
+++ incident pipeline/load_incident_data.py
@@ -0,0 +1,51 @@
+from os import environ, _Environ
+
+from dotenv import load_dotenv
+from psycopg2 import connect
+from psycopg2.extensions import connection
+
+from datetime import datetime, timedelta
+
+import pandas as pd
+from pandas import DataFrame
+
+
+def get_operator_info_df():
+
+    df = pd.read_html("https://en.wikipedia.org/wiki/List_of_companies_operating_trains_in_the_United_Kingdom",
+                      flavor="bs4", attrs={"class": "wikitable"})[0]
+    df["Passenger satisfaction[1]"] = df["Passenger satisfaction[1]"].apply(
+        lambda x: str(x).strip("%"))
+    df["Passenger satisfaction[1]"] = pd.to_numeric(
+        df["Passenger satisfaction[1]"], errors="coerce")
+    df = df.rename(
+        columns={"Passenger satisfaction[1]": "Passenger satisfaction (%)"})
+    df = df.drop([9, 21], axis=0)
+    additional_operators_df = pd.DataFrame({
+        "Operator": ["Gatwick Express", "Great Northern", "Southern", "Thameslink",
+                     "South Western Railway", "Island Line"],
+        "Code": ["GX", "GN", "SN", "TL", "SW", "IL"],
+        "Passenger satisfaction (%)": [80, 80, 80, 80, 75, 75]
+    })
+    df = pd.concat([df, additional_operators_df], ignore_index=True)
+    print(df[["Code", "Passenger satisfaction (%)"]])
+    return df
+
+
+def seed_operator_table(conn: connection, operator_info: DataFrame):
+
+    operator_info_values = operator_info[[
+        'Operator', 'Code', 'Passenger satisfaction (%)']].values.tolist()
+
+    with conn.cursor() as cur:
+
+        cur.executemany("""INSERT INTO operator 
+                    (operator_name,
+                    operator_code,
+                    passenger_satisfaction)
+                    VALUES
+                    (%s, %s, %s)
+                    ON CONFLICT DO NOTHING;
+                    """, operator_info_values)
+
+    conn.commit()
---
+++ incident pipeline/opendata-nationalrail-client.py
@@ -0,0 +1,130 @@
+#
+# National Rail Open Data client demonstrator
+# Copyright (C)2019-2022 OpenTrainTimes Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import stomp
+import zlib
+import io
+import time
+import socket
+import logging
+import xml.etree.ElementTree as ET
+import pandas as pd
+from pandas import DataFrame
+from datetime import datetime
+from extract_incident_data import (
+    extract_and_transform_incident_data,
+    convert_timestamp,
+    flatten_incident_data
+)
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s\t%(message)s', level=logging.INFO)
+
+try:
+    import PPv16
+except ModuleNotFoundError:
+    logging.error(
+        "Class files not found - please configure the client following steps in README.md!")
+
+USERNAME = 'KBb3ad148f-a9ed-46ca-a37e-b7f6a65aba76'
+PASSWORD = '8b998c5a-d74d-4bd5-a6a0-5bfb498a9b08'
+HOSTNAME = 'kb-dist-261e4f.nationalrail.co.uk'
+HOSTPORT = 61613
+# Always prefixed by /topic/ (it's not a queue, it's a topic)
+TOPIC = '/topic/kb.incidents'
+
+CLIENT_ID = socket.getfqdn()
+HEARTBEAT_INTERVAL_MS = 30000
+HEARTBEAT_RESPONSE_TIMEOUT = 25000
+RECONNECT_DELAY_SECS = 15
+
+namespaces = {
+    'ns2': 'http://nationalrail.co.uk/xml/common',
+    'ns3': 'http://nationalrail.co.uk/xml/incident'
+}
+
+if USERNAME == '':
+    logging.error(
+        "Username not set - please configure your username and password in opendata-nationalrail-client.py!")
+
+
+def connect_and_subscribe(connection):
+    if stomp.__version__[0] < 5:
+        connection.start()
+
+    connect_header = {'client-id': USERNAME + '-' + CLIENT_ID}
+    subscribe_header = {'activemq.subscriptionName': CLIENT_ID}
+
+    connection.connect(username=USERNAME,
+                       passcode=PASSWORD,
+                       wait=True,
+                       headers=connect_header)
+
+    connection.subscribe(destination=TOPIC,
+                         id='1',
+                         ack='auto',
+                         headers=subscribe_header)
+
+
+class StompClient(stomp.ConnectionListener):
+
+    def on_heartbeat(self):
+        logging.info('Received a heartbeat')
+
+    def on_heartbeat_timeout(self):
+        logging.error('Heartbeat timeout')
+
+    def on_error(self, headers, message):
+        logging.error(message)
+
+    def on_disconnected(self):
+        logging.warning(
+            'Disconnected - waiting %s seconds before exiting' % RECONNECT_DELAY_SECS)
+        time.sleep(RECONNECT_DELAY_SECS)
+        connect_and_subscribe(self.conn)
+
+    def on_connecting(self, host_and_port):
+        logging.info('Connecting to ' + host_and_port[0])
+
+    def on_message(self, frame):
+        try:
+            # print(frame.body.decode())
+            message_data = extract_and_transform_incident_data(
+                frame.body.decode(), namespaces)
+            flattened_msg = flatten_incident_data(message_data)
+            msg_df = pd.DataFrame(flattened_msg)
+            print(message_data)
+            print(msg_df)
+            input()
+        except Exception as e:
+            logging.error(str(e))
+
+
+conn = stomp.Connection12([(HOSTNAME, HOSTPORT)],
+                          auto_decode=False,
+                          )
+
+client = StompClient()
+client.conn = conn
+conn.set_listener('', client)
+connect_and_subscribe(conn)
+
+while True:
+    time.sleep(1)
+
+conn.disconnect()
